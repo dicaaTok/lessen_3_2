ee6a4331c68a9aaf3411ffbfe22cc9878b902a71
dicaaTok <dicaaGeek@gmail.com>
2025-03-22 16:20:27 +0600

    gg

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
         viewBinding = true
     }
     buildFeatures {
+        //noinspection DataBindingWithoutKapt
         dataBinding = true
     }
 }
@@ -54,6 +55,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+
 }
